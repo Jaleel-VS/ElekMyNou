56a05627ca98fc96d6cbdb5ef12b8e58eab83927
Jaleel-VS <jeremikress@gmail.com>
2024-02-09 17:47:45 +0200

    Add models for User, Voter, Candidate, Election, and Vote

---
+++ backend/models.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List
+
+class User(BaseModel):
+    user_id: str
+    username: str
+    first_name: str
+    last_name: str
+    email: str
+    address: str
+    nationalID: str
+
+class Voter(User):
+    has_voted: bool = False
+
+class Candidate(User):
+    party_name: str
+    manifesto: str
+
+class Election(BaseModel):
+    election_id: str
+    election_name: str
+    election_start: str
+    election_end: str
+
+class Vote(BaseModel):
+    vote_id: str
+    voter_id: str 
+    candidate_id: str  
+    vote_time: str
+    vote_location: str
+
+
