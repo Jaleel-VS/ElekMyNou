6952128caea254b2a10cd27555994c5c84f96fe8
Jaleel-VS <jeremikress@gmail.com>
2024-02-14 13:57:42 +0200

    Add password hashing and verification functions

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,7 @@
 from firebase_admin import auth
 from fastapi import Depends, HTTPException, status
+import cryptic
+import hashlib
 
 async def get_current_user(token: str = Depends(auth.verify_id_token)):
     user = auth.get_user(token)
@@ -11,3 +13,28 @@ async def get_current_user(token: str = Depends(auth.verify_id_token)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials",
         )
+    
+
+def hash_password(password: str):
+    salt = cryptic.SALT
+
+    hashed_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000);
+
+    return hashed_password.hex()
+
+def verify_password(plain_password: str, hashed_password: str):
+    
+    return hashed_password == hash_password(plain_password)
+
+if __name__ == "__main__":
+    # test the hash_password function
+
+    password = "password"
+
+    hashed_password = hash_password(password)
+
+    print(hashed_password)
+
+    # test the verify_password function
+
+    print(verify_password(password, hashed_password)) # should return True
